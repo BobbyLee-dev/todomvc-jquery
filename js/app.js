f3d159a857b51422da953d9b7171a8f8c3c22bf8
Bobby <rdlee8181@gmail.com>
2018-03-17 22:07:00 -0700

    adjusted editKeyUP

--- js/app.js
+++ js/app.js
@@ -163,8 +163,7 @@ jQuery(function ($) {}
 		},
 		editingMode: function (e) {
 			var $input = $(e.target).closest('li').addClass('editing').find('.edit');
-			var val = $input.val();
-			$input.val('').focus().val(val);
+			$input.focus().select();
 		},
 		editKeyup: function (e) {
 			if (e.which === ENTER_KEY) {
